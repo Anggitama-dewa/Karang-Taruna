da5aa3951871db7c01df4c5695770c8fdfc1a4a3
Anggitama-dewa <anggitamadewa@gmail.com>
2024-06-11 10:56:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Olympia Gym</title>
+<style type="text/css">
+<!--
+.style2 {
+	font-size: 36px;
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	color: #000000;
+}
+.style8 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 14px;
+}
+.style14 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 18px;
+	color: #000000;
+}
+.style15 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 18px; }
+.style18 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 18px; color: #FFFFFF; }
+-->
+</style>
+</head>
+
+<body>
+<table width="1353" height="1011" border="o" bordercolor="#FFFFFF" bgcolor="#FFFFFF">
+  <tr>
+    <td width="373" rowspan="2"><img src="images/Olympia.png" alt="logo" width="251" height="236" /></td>
+    <td height="224" colspan="3" align="left" bgcolor="#FFFFFF"><h1><span class="style2"> Aplikasi Web Olympia Gym</span></h1></td>
+    <td align="center" bgcolor="#FFFFFF"><span class="style14"><a href="login.php">Log in</a></span></td>
+  </tr>
+  <tr>
+    <td width="325" height="24" align="left" bgcolor="#FFFFFF" class="style14"><a href="index.php">Home</a></td>
+    <td width="280" align="left" bgcolor="#FFFFFF" class="style15"><a href="paket.php">Paket</a></td>
+    <td width="221" align="left" bgcolor="#FFFFFF"><span class="style15"><a href="index.php">Tentang Kami
+    </a></span></td>
+    <td width="178" align="left" bgcolor="#FFFFFF" class="style15">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="424" colspan="5"><img src="images/home2.jpg" alt="home" width="1393" height="423" /></td>
+  </tr>
+  <tr>
+    <td height="203" colspan="5" align="center"><p class="style15">Olympia Gym menyediakan fasilitas modern, program latihan yang beragam, dan pelatih profesional yang siap membantumu mencapai tujuan kebugaranmu. Bergabunglah dengan komunitas kami yang suportif dan jadikan gaya hidup sehat sebagai prioritasmu. Raih kesehatan prima dan bentuk tubuh idealmu di Olympia Gym!</p>    </td>
+  </tr>
+  <tr>
+    <td height="27" colspan="5" align="center" bgcolor="#243F71"><span class="style18">Contact Info </span></td>
+  </tr>
+  <tr>
+    <td height="57" colspan="2" align="center"><p class="style15">Email Support</p>
+    <p class="style15">olympiafitness@xxx.xxx </p></td>
+    <td colspan="3" align="center"><p class="style15">Call Center</p>
+    <p class="style15">081xx-xxxx-xxxx </p></td>
+  </tr>
+  <tr>
+    <td height="27" colspan="5" align="center"><span class="style8">Copyright &copy; 2024 Olympia Gym </span></td>
+  </tr>
+</table>
+</body>
+</html>
